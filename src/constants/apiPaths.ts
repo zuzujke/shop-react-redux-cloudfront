33556ebd53e70e0f105a3205cde8c5daa3ac2c6e
Kostiantyn Semenenko <ksemenenko@delhaize.be>
2023-10-08 20:48:02 +0300

    Add AWS API

--- src/constants/apiPaths.ts
+++ src/constants/apiPaths.ts
@@ -1,8 +1,9 @@
 const API_PATHS = {
-  product: "https://.execute-api.eu-west-1.amazonaws.com/dev",
+  product:
+    "https://o3ftf9ru30.execute-api.eu-west-1.amazonaws.com/dev/products",
   order: "https://.execute-api.eu-west-1.amazonaws.com/dev",
   import: "https://.execute-api.eu-west-1.amazonaws.com/dev",
-  bff: "https://.execute-api.eu-west-1.amazonaws.com/dev",
+  bff: "https://o3ftf9ru30.execute-api.eu-west-1.amazonaws.com/dev",
   cart: "https://.execute-api.eu-west-1.amazonaws.com/dev",
 };
 
